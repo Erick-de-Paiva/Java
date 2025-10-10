91d2b9066cf98ec351a0636405e9afc175b485a5
Erick <paiva.erick2001@gmail.com>
2025-10-10 16:03:33 -0300

    exercicio 01

---
+++ java_exercicios/src/java_exercicios/exercicio_01.java
@@ -0,0 +1,30 @@
+package java_exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_01 {
+
+	public static void main(String[] args) {
+		
+		//1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
+		
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.US);
+		
+		float numero1, numero2;
+		
+		System.out.print("Escreva o primeiro número: ");
+		numero1 = sc.nextFloat();
+		
+		System.out.print("Escreva o segundo número: ");
+		numero2 = sc.nextFloat();
+		
+		float resultado = numero1 + numero2;
+		
+		System.out.print("Resultado da soma: " + resultado);
+		
+		sc.close();
+	}
+
+}
