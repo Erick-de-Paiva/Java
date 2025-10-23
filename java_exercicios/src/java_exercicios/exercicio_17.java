1ea10e4e84922761994ab73280cf233574f5ef1d
Erick <paiva.erick2001@gmail.com>
2025-10-23 14:24:13 -0300

    exercicio 17

---
+++ java_exercicios/src/java_exercicios/exercicio_17.java
@@ -0,0 +1,38 @@
+package java_exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_17 {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * 17. Leia 80 números e ao final informar quantos número(s) est(á)ão no intervalo entre 10 (inclusive) e 150 
+		 * (inclusive);
+		 */
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		float[]n = new float[80];
+		int contador = 0;
+		
+		for (int i = 0; i < 80; i++) {
+			System.out.println("Escreva o numero " + (i+1) + ":");
+			n[i] = sc.nextFloat();
+			
+			if (n[i] >= 10 && n[i] <= 150) {
+				contador = contador +1;
+			}
+			
+		}
+		
+		System.out.println();
+		System.out.println("Quantidade de numeros no intervalo entre 10 e 150: " + contador);
+		
+		sc.close();
+		
+	}
+
+}
